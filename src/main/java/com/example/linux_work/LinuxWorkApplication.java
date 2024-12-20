5b42de3c20ac2b1643c0708d01efe7483137b4df
NickBai007 <3424846024@qq.com>
2024-12-20 15:55:38 +0800

    first submission

---
+++ src/main/java/com/example/linux_work/LinuxWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.linux_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxWorkApplication.class, args);
+    }
+
+}
